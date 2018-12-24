a9aec56d74191038dc8ec42a58c5b8169ebde053
Dane Johnson <daneallenjohnson@protonmail.com>
2018-12-23 18:41:42 -0600

    Added more to call

--- src/cubs_sign/core.clj
+++ src/cubs_sign/core.clj
@@ -4,6 +4,13 @@
             [clojure.data.json :as json])
   (:gen-class))
 
+;;;;;;;;;;;;;;;;;;;
+;; Some globals. ;;
+;;;;;;;;;;;;;;;;;;;
+
+(def cubs-score (atom -1))
+(def opponent-score (atom -1))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Retrieve data on the Chicago Cubs and do something with it. ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -25,23 +32,50 @@
 (defn get-cubs-data
   "Throw away games that don't involve the Cubbies"
   [data]
-  (-> data
-      (get-in ["scoreboard" "gameScore"])
-      (->> (filter #(or (= (get-in % ["game" "homeTeam" "ID"]) *cubs-id*)
-                        (= (get-in % ["game" "awayTeam" "ID"]) *cubs-id*))))))
+  (let [gamedata
+        (-> data
+            (get-in ["scoreboard" "gameScore"])
+            (->> (filter #(or (= (get-in % ["game" "homeTeam" "ID"]) *cubs-id*)
+                              (= (get-in % ["game" "awayTeam" "ID"]) *cubs-id*))))
+            (last))]
+    (cond
+      (nil? gamedata) (do (reset! cubs-score -1)
+                          (reset! opponent-score -1))
+      (= (get-in gamedata ["game" "homeTeam" "ID"] *cubs-id*))
+      (do
+        (reset! cubs-score (Integer/parseInt (get gamedata "homeScore")))
+        (reset! opponent-score (Integer/parseInt (get gamedata  "awayScore"))))
+      :default
+      (do
+        (reset! cubs-score (Integer/parseInt (get gamedata  "awayScore")))
+        (reset! opponent-score (Integer/parseInt (get gamedata  "homeScore")))))
+    [@cubs-score @opponent-score]))
 
 (defn get-scoreboards
   "Retrieve the all game scoreboards from the server."
   []
-  (GET "mlb/current/scoreboard.json?fordate=2018081"
+  (GET (str "mlb/current/scoreboard.json?fordate=" (.format (java.text.SimpleDateFormat. "yyyyMMdd")
+                                                            (java.util.Date.))
+            "&force=false")
        (fn [{:keys [status body error]}]
          (if (= status 200)
            (println (get-cubs-data (json/read-str body)))
-           (println error)))))
+           (println status)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Go Cubs Go!"))
+  (while true
+    (get-scoreboards)
+    (Thread/sleep (* 15 1000))))
 
 
+;;;;;;;;;;;
+;; DEBUG ;;
+;;;;;;;;;;;
+
+(def mockdata
+  {"scoreboard" {"gameScore" [{"game" {"awayTeam" {"ID" *cubs-id*}
+                                       "homeTeam" {"ID" "666"}}
+                               "homeScore" "7"
+                               "awayScore" "10"}]}})
